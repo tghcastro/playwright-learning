e9aac6349c4636d3bcc4462b98448e3887f0fe58
Thiago de Castro <tghcastro@gmail.com>
2022-06-02 21:52:35 +0100

    using "fill" to signin with correct credentials

--- test-1/tests/UIBasicTests.spec.js
+++ test-1/tests/UIBasicTests.spec.js
@@ -1,22 +1,26 @@
 const {test, expect} = require('@playwright/test')
 
+const wrongUsername = 'rahulshetty'
+const correctUsername = 'rahulshettyacademy'
+const siteUrl = 'https://www.rahulshettyacademy.com/loginpagePractise/' 
+
 test('Setting browser context test', async ({browser})=> {
     // It's possible to set Cookies and other bowser using context
     const context = await browser.newContext()
     const page = await context.newPage()
-    await page.goto('https://www.rahulshettyacademy.com/loginpagePractise/')
+    await page.goto(siteUrl)
     console.log(await page.title())
     await expect(page).toHaveTitle('Wrong title to make test fail')
 })
 
 test('Default browser context test', async ({page}) => {
     // Context from previous test is the default configuration
-    await page.goto('https://www.rahulshettyacademy.com/loginpagePractise/')
+    await page.goto(siteUrl)
     await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy')
 })
 
 test('Signin with wrong credentials', async ({page}) => {
-    await page.goto('https://www.rahulshettyacademy.com/loginpagePractise/')
+    await page.goto(siteUrl)
     
     const wrongUsername = 'rahulshetty'
 
@@ -31,4 +35,29 @@ test('Signin with wrong credentials', async ({page}) => {
     const  errorMessage = page.locator("[style*='block']")
     console.log("Error message: " + await errorMessage.textContent())
     await expect(errorMessage).toContainText("Incorrect username/password.")
+})
+
+
+test('Signin with correct credentials', async ({page}) => {
+    await page.goto(siteUrl)
+    
+    const username = await page.locator('#username')
+    await username.type(wrongUsername)
+
+    const password = await page.locator('#password')
+    await password.type('learning')
+
+    const signinButton = await page.locator('#signInBtn')
+    await signinButton.click()
+
+    const  errorMessage = page.locator("[style*='block']")
+    console.log("Error message: " + await errorMessage.textContent())
+    await expect(errorMessage).toContainText("Incorrect username/password.")
+
+    await username.fill(correctUsername) // fill method replaces previously written content
+    await signinButton.click()
+
+    const top = page.locator('body > app-root > app-navbar > div > nav > a')
+    await expect(top).toContainText('ProtoCommerce')
+    
 })
